6f4f20780f8ccdee53f8438278f50f45e6f58640
Erhan Arda <erhanarda7@gmail.com>
2020-05-01 00:24:29 +0300

    reactstrap card added

--- src/containers/DogDetails.js
+++ src/containers/DogDetails.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import dogs from '../dogsdata'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
+import "./styles.css";
 
 const DogDetails = (props) => {
     const selectedDog = props.match.params.writtenDogId;
@@ -7,20 +12,18 @@ const DogDetails = (props) => {
 
     return (
         <div>
-            <li>
-                <h1> selectedDog:  {showDogs.name}</h1>
-                <ul>
-                    <li>{showDogs.description}
-                        <li>
-                            {showDogs.age}
-                        </li>
-                        <li>
-                            {showDogs.breed}
-                        </li>
-                    </li>
-                </ul>
-            </li>
-        </div>
+            <Card className="card">
+                    <CardImg width="100%" src={showDogs.image} alt="Card image cap" />
+                    <CardBody >
+                        <CardTitle>
+                            <h3>{showDogs.name}</h3>
+                        </CardTitle>
+                        <CardSubtitle>{showDogs.age}</CardSubtitle>
+                        <CardText>{showDogs.description}</CardText>
+                    </CardBody>
+                </Card>
+    </div>
+        
     )
 }
 
