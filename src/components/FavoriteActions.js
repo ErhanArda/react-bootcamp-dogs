e0f44b7ffb0efb044a29e3b6216b2c4029bdbedf
Erhan Arda <erhanarda7@gmail.com>
2020-05-02 23:41:43 +0300

    favorite button disabled function added

--- src/components/FavoriteActions.js
+++ src/components/FavoriteActions.js
@@ -1,22 +1,17 @@
-import React from 'react'
-import { Button } from 'reactstrap'
+import React from 'react';
+import { Button } from "reactstrap";
 
 const FavoriteActions = (props) => {
+
     return (
         <div>
             {
                 props.getStatus(props.id) ?
-                    <Button color="danger"
-                        onClick={() => { props.toggle(props.id) }}>
-                        Remove Favorite
-                        </Button>
-                    : <Button color="primary"
-                        onClick={() => { props.toggle(props.id) }}>
-                        Add Favorite
-                        </Button>
+                    <Button color="danger" disabled={props.id === props.disabledButton} onClick={() => { props.toggle(props.id) }}>Favorilerden Cikar</Button>
+                    : <Button color="primary" disabled={props.id === props.disabledButton} onClick={() => { props.toggle(props.id) }}>Favoriye Ekle</Button>
             }
         </div>
-    )
-}
+    );
+};
 
-export default FavoriteActions
+export default FavoriteActions;
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,85 +1,100 @@
-import React, { Component } from 'react'
-import dogs from '../dogsdata';
-import Dog from '../components/Dog'
-import axios from 'axios'
+import React from 'react';
+import dogs from "../dogsdata";
+// import {Button} from "reactstrap";
+// import FavoriteActions from "../components/FavoriteActions";
+import Dog from "../components/Dog";
+import axios from "axios";
+
 
 const apiHost = "https://5ea56a222d86f00016b45ccd.mockapi.io"
-class HomePage extends Component {
 
+class Homepage extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
-            favorites: []
+            favorites: [],
+            loadingFavorites: false,
+            disabledButton: "",
         }
     }
-
     componentDidMount() {
-        /* local storage 
-        this.setState({
-            favorites:window.localStorage.getItem("favorites")?
-            JSON.parse(window.localStorage.getItem("favorites")): []
-        })
+        // localstoragedan getirme
+        /*        this.setState({
+                    favorites: window.localStorage.getItem("favorites") ? JSON.parse(window.localStorage.getItem("favorites")): []
+                })*/
 
-        */
-        axios.get(`${apiHost}/favorites`).then((result) => {
-            this.setState({
-                favorites: result.data
+        this.setState({
+            loadingFavorites: true
+        }, () => {
+            axios.get(`${apiHost}/favorites`).then((result) => {
+                this.setState({
+                    favorites: result.data,
+                    loadingFavorites: false
+                })
+            }).catch((err) => {
+                console.log("Axios err", err);
+                this.setState(({
+                    loadingFavorites: false
+                }))
             })
-        }).catch((err) => {
-            console.log("Axios err", err)
         })
     }
 
-
-
-
-
     toggle = (dogId) => {
-        const foundDog = this.state.favorites.find((favorite) => favorite.dogId === dogId)
+        this.setState({
+            disabledButton: dogId//disabled
+        })
+        const foundDog = this.state.favorites.find((favorite) => favorite.dogId === dogId);
         if (foundDog) {
-            this.setState({
-                favorites: this.state.favorites.filter((dog) => dog.dogId !== dogId)
-            }, () => {
-                // window.localStorage.setItem("favorites",JSON.stringify(this.state.favorites));
-            })
+            axios.delete(`${apiHost}/favorites/${foundDog.id}`).then((result) => {
+                this.setState(({
+                    favorites: this.state.favorites.filter((dog) => dog.dogId !== dogId),
+                    disabledButton: false,
+                }))
+            }).catch((err) => {
+                console.log(err);
+            });
         } else {
-            this.setState({
-                favorites: [...this.state.favorites, dogId]
-            }, () => {
-                //window.localStorage.setItem("favorites", JSON.stringify(this.state.favorites));
-                axios.post(`${apiHost}/favorites`, {
-                    dogId
-                }).then((result) => {
-                    const addedFav = result.data
-                    this.setState({
-                        favorites: [...this.state.favorites, addedFav]
-                    })
-                }).catch((err) => {
-                    console.log(err)
+            // window.localStorage.setItem("favorites", JSON.stringify(this.state.favorites));
+            axios.post(`${apiHost}/favorites`, {
+                dogId
+            }).then((result) => {
+                const eklenenFavori = result.data; // {id: 1, dogId: benim yolladigim dog id, createdat: date}
+                this.setState({
+                    favorites: [...this.state.favorites, eklenenFavori],
+                    disabledButton: false,
+
                 })
+            }).catch((err) => {
+                console.log(err);
             })
         }
     }
 
     getStatus = (dogId) => {
-        const foundDog = this.state.favorites.find((favorite) => favorite === dogId)
-        return foundDog
+        const foundDog = this.state.favorites.find((favorite) => favorite.dogId === dogId);
+        return foundDog;
     }
 
     render() {
+        if (this.state.loadingFavorites) {
+            return <div>
+                <h1>Sayfa Yukleniyor.....</h1>
+            </div>
+        }
         return (
             <div>
                 <ul>
                     {
                         dogs.map((dog) => {
-                            return <Dog id={dog.id} toggle={this.toggle} getStatus={this.getStatus} {...dog} />
+                            return <Dog toggle={this.toggle} id={dog.id} getStatus={this.getStatus} disabledButton={this.state.disabledButton} {...dog} />
                         })
                     }
                 </ul>
             </div>
-        )
+        );
     }
 }
 
-export default HomePage
+export default Homepage;
