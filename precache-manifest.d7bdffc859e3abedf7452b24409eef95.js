2077c8377bf948618191b29fcbf68edb7e41c7e6
Erhan Arda <erhanarda7@gmail.com>
2020-05-02 23:48:19 +0300

    Updates

---
+++ precache-manifest.d7bdffc859e3abedf7452b24409eef95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7503ff11b24a12374c3d654b5037aa",
+    "url": "/react-bootcamp-dogs/index.html"
+  },
+  {
+    "revision": "7b29600e31a4d1dd5e50",
+    "url": "/react-bootcamp-dogs/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b6a196aebd74da3f1ef7",
+    "url": "/react-bootcamp-dogs/static/css/main.4320ffbe.chunk.css"
+  },
+  {
+    "revision": "7b29600e31a4d1dd5e50",
+    "url": "/react-bootcamp-dogs/static/js/2.12045a22.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-bootcamp-dogs/static/js/2.12045a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a196aebd74da3f1ef7",
+    "url": "/react-bootcamp-dogs/static/js/main.2e1fb926.chunk.js"
+  },
+  {
+    "revision": "2be0a1e9409095988623",
+    "url": "/react-bootcamp-dogs/static/js/runtime-main.8eaeebfe.js"
+  }
+]);
